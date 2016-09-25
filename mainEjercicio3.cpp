a002815483d732d47d7b43c016575375500ed083
Alejandro <alejo.rosales95@gmail.com>
2016-09-25 16:23:12 -0400

    Add files via upload

---
+++ mainEjercicio3.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	int x, y, z;
+	
+	printf ("Ejercicio 3\nLeer tres numeros y escribir el mayor de los tres");
+	printf ("\n------------------------------------------------");
+	
+	printf ("\n\nEscribe el 1er Numero :");
+	scanf ("%d",&x);
+	
+	printf ("\nEscribe el 2do Numero :");
+	scanf ("%d",&y);
+	
+	printf ("\nEscribe el 3er Numero :");
+	scanf ("%d",&z);
+	
+	if (x > y ){
+		if (x > z){
+			printf ("\n\nEl Numero %d es el mayor ",x);
+		}else{
+			printf ("\n\n El Numero %d es el mayor ",z);			
+		}
+	}
+	
+	if (y > x){
+		if (y > z){
+			printf ("\n\nEl Numero %d es el mayor ",y);			
+		}else{
+			printf ("\n\nEl Numero %d es el mayor ",z);
+		}
+	}
+
+	return 0;
+}
